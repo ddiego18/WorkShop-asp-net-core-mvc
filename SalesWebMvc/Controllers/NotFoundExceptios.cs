f3ba415f9cf2567169d8db8ec109a9b35f477c7f
ddiego18 <ddiego18@gmail.com>
2021-08-01 20:56:06 -0300

    Subir código

---
+++ SalesWebMvc/Controllers/NotFoundExceptios.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SalesWebMvc.Controllers
+{
+    [Serializable]
+    internal class NotFoundExceptios : Exception
+    {
+        public NotFoundExceptios()
+        {
+        }
+
+        public NotFoundExceptios(string message) : base(message)
+        {
+        }
+
+        public NotFoundExceptios(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NotFoundExceptios(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
